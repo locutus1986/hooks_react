7e10f86fc498ac0d935ac6e4fbe1e19011123fa6
locutus <locutus@localhost.localdomain>
2021-02-20 20:33:29 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accorion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+    {
+        title: 'what is Ract?',
+        content: 'React is a front end javascript framwork'
+    },
+    {
+        title: 'why use React',
+        content: 'React is a favorite js library'
+    },
+    {
+        title: 'How do you use React',
+        content: 'You use React by creating componets'
+    }
+]
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return (
+    <div>
+        <Accordion items={items}/>
     </div>
-  );
-}
-
-export default App;
+    )
+};
